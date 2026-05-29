f0f6d290e2fdce002e8cedf39acbe7bfffd242fd
coert <coert.vangemeren@hu.nl>
2026-05-29 22:16:05 +0200

    Add timeline test script

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# uv run python swin2_classifier.py \
+#   --mode test \
+#   --test-data generated/rodent_annotations_test.csv \
+#   --checkpoint runs/swin2-classifier/best.pt
+
+uv run python recode_classifier_timeline.py \
+  --input-dir "generated/swin2_classifier_video_test" \
+  --bar-height 30 \
+  --slider-width 3 \
+  --ground-truth-csv "Translational neuroimaging group - rodents/video_data.csv" \
+  --annotations-csv "generated/rodent_annotations_test.csv" \
+  --prefix-buffer-frames 5 \
+  --postfix-buffer-frames 5
